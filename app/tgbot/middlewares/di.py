a7f2d8ae98b311b1b82e22a1eeb4d2822a4c8b2c
Kudyakov Artem <t3m8ch@yandex.ru>
2022-02-28 23:07:14 +0400

    feat(tgbot): Create middleware for dependency injection

---
+++ app/tgbot/middlewares/di.py
@@ -0,0 +1,28 @@
+from typing import Callable, Dict, Any, Awaitable
+
+from aiogram import BaseMiddleware
+from aiogram.types import TelegramObject
+from sqlalchemy.orm import sessionmaker
+
+from app.core.impl.services.teams import TeamsServiceImpl
+
+
+class DIMiddleware(BaseMiddleware):
+    def __init__(self, session_maker: sessionmaker):
+        self._session_maker = session_maker
+
+    async def __call__(
+            self,
+            handler: Callable[
+                [TelegramObject, Dict[str, Any]], Awaitable[Any]
+            ],
+            event: TelegramObject,
+            data: Dict[str, Any]
+    ) -> Any:
+        session = self._session_maker()
+
+        try:
+            data["teams_service"] = TeamsServiceImpl(session)
+            return await handler(event, data)
+        finally:
+            await session.close()
